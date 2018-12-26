0478b61872f82e5fcebe551a09001a04ab7782a9
Richard <richard@stromer.org>
2018-12-26 09:42:10 +0100

    make whole program promise safe

--- index.js
+++ index.js
@@ -7,17 +7,22 @@ const startPath = '/Volumes/Intenso External USB 3.0 Media/Photos';
 const main = async basePath => {
   const files = await fs.readdir(basePath);
 
-  files.forEach(async file => {
-    const filePath = path.join(basePath, file);
+  return Promise.all(
+    files.map(async file => {
+      const filePath = path.join(basePath, file);
 
-    const stats = await fs.lstat(filePath);
-    if (stats.isDirectory()) {
-      main(filePath);
-    } else {
-      const exifTags = await exiftool.read(filePath);
-      console.log(filePath, exifTags);
-    }
-  });
+      const stats = await fs.lstat(filePath);
+      if (stats.isDirectory()) {
+        await main(filePath);
+      } else {
+        const exifTags = await exiftool.read(filePath);
+        console.log(filePath, exifTags.CreateDate);
+      }
+    }),
+  );
 };
 
-main(startPath);
+(async () => {
+  await main(startPath);
+  console.log('Done');
+})();
