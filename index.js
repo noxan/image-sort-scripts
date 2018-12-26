c44ef2d33014b477586a548d4f5715ac0ad7c1ae
Richard <richard@stromer.org>
2018-12-26 09:33:35 +0100

    try to list all files of directory recursive

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+const fs = require('fs');
+const path = require('path');
 const exiftool = require('exiftool-vendored').exiftool;
+
+const startPath = '/Volumes/Intenso External USB 3.0 Media/Photos';
 
 exiftool
   .version()
   .then(version => console.log(`We're running ExifTool v${version}`));
+
+const main = basePath => {
+  const directories = fs.readdirSync(basePath);
+
+  directories.forEach(directory => {
+    const directoryPath = path.join(basePath, directory);
+    const stats = fs.lstatSync(directoryPath);
+    if (stats.isDirectory) {
+      main(directoryPath);
+    }
+  });
+
+  console.log(directories);
+};
+
+main(startPath);
